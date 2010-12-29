922c2dbc0a8807af8518fc3ddd1bbcef5087e036
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-29 01:36:46 +0100

    TODO

--- 02-flyspell.el
+++ 02-flyspell.el
@@ -7,11 +7,13 @@
 ;; sort corrections
 (setq flyspell-sort-corrections nil)
 
+;; TODO: use (add-hook 'a-mode-hook 'flyspell-mode)
 (defun enable-flyspell-for (hook)
   "Helper to enable `flyspell-mode' in hook."
   (add-hook hook (lambda ()
 		    (flyspell-mode))))
 
+;; TODO: use (add-hook 'a-mode-hook 'flyspell-prog--mode)
 (defun enable-prog-flyspell-for (hook) 
   "Helper to enable `flyspell-prog-mode' in hook."
   (add-hook hook (lambda ()
