4d931442811afc609897d5d9b77233933f4daa19
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-29 02:03:58 +0100

    replaced custom functions with more flexible skeleton-pair-insert-maybe

--- 40-auto-parens.el
+++ 40-auto-parens.el
@@ -1,27 +1,27 @@
 ;; handy insert-pair extensions for elisp/clojure
-;; TODO: can be replaced with
-;; (setq skeleton-pair t)
-;; (global-set-key (kbd "(") 'skeleton-pair-insert-maybe)
-;; (global-set-key (kbd "[") 'skeleton-pair-insert-maybe)
-;; (global-set-key (kbd "{") 'skeleton-pair-insert-maybe)
-;; (global-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
+(setq skeleton-pair t)
+(global-set-key (kbd "(") 'skeleton-pair-insert-maybe) 
+(global-set-key (kbd "[") 'skeleton-pair-insert-maybe)
+(global-set-key (kbd "{") 'skeleton-pair-insert-maybe)
+(global-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
 
-(defun insert-brackets ()
-  (interactive)
-  (insert-pair 0 ?[ ?]))
-
-(defun insert-braces ()
-  (interactive)
-  (insert-pair 0 ?{ ?}))
-
-(defun insert-double-quotes ()
-  (interactive)
-  (insert-pair 0 ?\" ?\"))
-
-(local-set-key "(" 'insert-parentheses)
-(local-set-key "[" 'insert-brackets)
-(local-set-key "{" 'insert-braces)
-(local-set-key "\"" 'insert-double-quotes)
+;; old functions, superseded by
+;; (defun insert-brackets ()
+;;   (interactive)
+;;   (insert-pair 0 ?[ ?]))
+;;
+;; (defun insert-braces ()
+;;   (interactive)
+;;   (insert-pair 0 ?{ ?}))
+;;
+;; (defun insert-double-quotes ()
+;;   (interactive)
+;;   (insert-pair 0 ?\" ?\"))
+;;
+;; (local-set-key "(" 'insert-parentheses)
+;; (local-set-key "[" 'insert-brackets)
+;; (local-set-key "{" 'insert-braces)
+;; (local-set-key "\"" 'insert-double-quotes)
 
 
 
