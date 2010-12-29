6e743e44914bc052b8732a4cfc05ca9e15ca7043
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-29 01:45:44 +0100

    fix: use / in place of \\ in path

--- 95-ms-windows.el
+++ 95-ms-windows.el
@@ -2,7 +2,7 @@
 ;; Windows specific stuff.
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2010-12-28 00:42:17 dfa>
+;; Time-stamp: <2010-12-29 01:45:34 dfa>
 
 (when (string-equal system-type "windows-nt")
     ;; setting homedir
@@ -10,4 +10,4 @@
 
     ;; setting ispell bin
     (setq ispell-program-name 
-	  "C:\\Program Files (x86)\\Aspell\\bin\\aspell.exe"))
+	  "C:/Program Files (x86)/Aspell/bin/aspell.exe"))
