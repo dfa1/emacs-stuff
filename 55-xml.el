3d984aa3a4ebe7d8fea3ea711d2f4427095d944c
Davide Angelocola <davide.angelocola@gmail.com>
2011-02-27 18:00:49 +0100

    enh: using nxml mode for XML/XSD files

---
+++ 55-xml.el
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; XML stuff
+;;
+;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
+;; Time-stamp: <2011-02-27 17:59:43 dfa>
+
+(add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.xsd$" . nxml-mode))
