f782f17cdd63e32916f1e1e6ac0edfd97936c3c5
Davide Angelocola <davide.angelocola@emaze.net>
2011-05-16 16:52:10 +0200

    ref: removed python-mode from https://launchpad.net/python-mode

--- 48-python.el
+++ 48-python.el
@@ -4,18 +4,6 @@
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
 ;; Time-stamp: <2011-05-11 11:16:50 dangelocola>
 
-(load-library "python-mode")
-
-;; (load-library "pycomplete") ;; requires pymacs
-(setq auto-mode-alist 
-      (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist 
-      (cons '("python" . python-mode) interpreter-mode-alist))
-
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-
-(global-font-lock-mode t)
-(setq font-lock-maximum-decoration t)
 (defun open-bpython ()
   (interactive)
 
