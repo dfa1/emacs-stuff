5dd04ed89181463e20cfc5592bd1b552769919e5
Davide Angelocola <davide.angelocola@emaze.net>
2011-05-17 11:35:49 +0200

    enh: enabling flyspell in python

--- 48-python.el
+++ 48-python.el
@@ -2,7 +2,7 @@
 ;; python-mode configuration (https://launchpad.net/python-mode)
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-05-11 11:16:50 dangelocola>
+;; Time-stamp: <2011-05-17 11:35:32 dfa>
 
 (defun open-bpython ()
   (interactive)
@@ -13,5 +13,6 @@
 
 (add-hook 'python-mode-hook
 	  (lambda ()
+	    (flyspell-mode 1)
 	    (local-set-key [f3] 'open-bpython)))
 
