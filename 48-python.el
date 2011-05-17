457b21d76a02e049485aae70494c2c6f63489ac5
Davide Angelocola <davide.angelocola@emaze.net>
2011-05-17 11:39:44 +0200

    fix: using flyspell-prog-mode in place of flyspell-mode

--- 48-python.el
+++ 48-python.el
@@ -2,7 +2,7 @@
 ;; python-mode configuration (https://launchpad.net/python-mode)
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-05-17 11:35:32 dfa>
+;; Time-stamp: <2011-05-17 11:39:24 dfa>
 
 (defun open-bpython ()
   (interactive)
@@ -13,6 +13,6 @@
 
 (add-hook 'python-mode-hook
 	  (lambda ()
-	    (flyspell-mode 1)
+	    (flyspell-prog-mode)
 	    (local-set-key [f3] 'open-bpython)))
 
