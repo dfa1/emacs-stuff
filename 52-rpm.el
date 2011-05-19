f21b0debae9941c22c37072e33f4f0cecae5f77a
Davide Angelocola <davide.angelocola@emaze.net>
2011-05-19 11:01:41 +0200

    enh: opening .spectemplate files using rpm-spec-mode

--- 52-rpm.el
+++ 52-rpm.el
@@ -2,10 +2,12 @@
 ;; RPM spec mode
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-04-28 13:55:03 dangelocola>
+;; Time-stamp: <2011-05-19 11:00:50 dfa>
 
 ;; TODO: use autoload
 (load-library "rpm-spec-mode")
 
-(setq auto-mode-alist (append '(("\\.spec" . rpm-spec-mode))
- 			       auto-mode-alist))
+(setq auto-mode-alist 
+      (append '(("\\.spec" . rpm-spec-mode) 
+		("\\.spectemplate" . rpm-spec-mode))
+	      auto-mode-alist))
