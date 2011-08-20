4b69ac47919dab4e4242f0c1510e23bad4208737
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-20 13:40:46 +0200

    ref: simplified clojure/swank/slime setup

--- 42-clojure.el
+++ 42-clojure.el
@@ -2,7 +2,10 @@
 ;; clojure mode
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2010-12-29 17:00:50 dfa>
+;; Time-stamp: <2011-08-20 12:53:13 dfa>
+
+(require 'clojure-mode)
+;; (require 'clojure-test-mode)
 
 (add-hook 'clojure-mode-hook
 	  (lambda ()
@@ -13,44 +16,18 @@
 
 	    (local-set-key (kbd "<tab>") 'complete-clojure)))
 
-; slime-repl customisation
-(add-hook 'slime-repl-hook
- (lambda ()
-   (slime-repl-eval-string "(set! *print-length* 10)")
-   (slime-repl-eval-string "(use 'clojure.contrib.repl-utils)")))
-
-;; slime (mapped to F3)
-(defun force-swith-to-slime() 
-  (interactive)
-  (if (slime-connected-p)
-    (slime-switch-to-output-buffer)
-    (slime)))
-
 ;; enable flyspell-prog-mode
 (add-hook 'clojure-mode-hook 'flyspell-prog-mode)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; clojure-mode without clojure-box 
-;;
-;; http://wiki.unto.net/setting-up-clojure-and-slime
-;; http://riddell.us/ClojureWithEmacsSlimeSwankOnUbuntu.html
-
-;; (add-to-list 'load-path "~/apps/clojure-mode")
-;; (add-to-list 'load-path "~/apps/swank-clojure")
-;; (add-to-list 'load-path "~/apps/slime")
-
-;; (setq swank-clojure-jar-path "~/apps/clojure/1.1/clojure.jar")
-
-;; (require 'clojure-mode)
-;; (require 'swank-clojure)
-;; (require 'slime)
-
-
-;; (eval-after-load "slime" (slime-setup '(slime-repl)))
-;; (slime-setup)
-
-;; ; slime-repl customisation
+; slime-repl customisation
 ;; (add-hook 'slime-repl-hook
 ;;  (lambda ()
-;;    (slime-repl-eval-string "(set! *print-length* 10)")
+;;    (slime-repl-eval-string "(set! *print-length*  swank-clojure 1.3.110)")
 ;;    (slime-repl-eval-string "(use 'clojure.contrib.repl-utils)")))
+
+;; slime (mapped to F3)
+;; (defun force-swith-to-slime() 
+;;   (interactive)
+;;   (if (slime-connected-p)
+;;     (slime-switch-to-output-buffer)
+;;     (slime)))
