790ce438d41c5f91d222376030b396402dc684dd
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-10 00:52:27 +0200

    ref: removed useless code

--- 08-auto-parens.el
+++ 08-auto-parens.el
@@ -2,7 +2,7 @@
 ;; Insert pair of (), [], {} and "", generally useful in all modes
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2010-12-29 16:55:09 dfa>
+;; Time-stamp: <2011-09-10 00:52:19 dfa>
 
 (setq skeleton-pair t)
 (global-set-key (kbd "(") 'skeleton-pair-insert-maybe) 
@@ -10,25 +10,3 @@
 (global-set-key (kbd "{") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
 
-;; old functions, superseded by
-;; (defun insert-brackets ()
-;;   (interactive)
-;;   (insert-pair 0 ?[ ?]))
-;;
-;; (defun insert-braces ()
-;;   (interactive)
-;;   (insert-pair 0 ?{ ?}))
-;;
-;; (defun insert-double-quotes ()
-;;   (interactive)
-;;   (insert-pair 0 ?\" ?\"))
-;;
-;; (local-set-key "(" 'insert-parentheses)
-;; (local-set-key "[" 'insert-brackets)
-;; (local-set-key "{" 'insert-braces)
-;; (local-set-key "\"" 'insert-double-quotes)
-
-
-
-
- 
