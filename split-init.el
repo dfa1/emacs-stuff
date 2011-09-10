802048c9ec9d74a8fa1ddffebe7ccec9c49fc218
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-10 23:50:18 +0200

    enh: compile-time unit tests for init-file-p

--- split-init.el
+++ split-init.el
@@ -2,7 +2,7 @@
 ;; rc.d like, init loader for GNU Emacs.
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-09-10 00:56:05 dfa>
+;; Time-stamp: <2011-09-10 23:49:17 dfa>
 
 (defun load-split-init (path) 
   "Load (via `load-file') an ordered set of files in way similar
@@ -18,7 +18,17 @@
 
 (defun init-file-p (path)
   "Returns nil if the path is an elisp file starting two-digits."
-  (string-match-p "^[0-9][0-9]-.*\.el$" (file-name-nondirectory path)))
+  (eq (string-match-p "^[0-9][0-9]-.+\.el$" (file-name-nondirectory path)) 0))
+
+(eval-when-compile 
+  (defmacro accept (value) 
+    `(assert ,value)) 
+  (defmacro refuse (value)
+    `(assert (not ,value)))
+  (accept (init-file-p "02-base.el"))
+  (accept (init-file-p "/home/user/02-base.el"))
+  (refuse (init-file-p "02-.el"))
+  (refuse (init-file-p "foo.el")))
 
 (defun discover-init-files (path)
   "Returns a sorted list of elisp files that starts with two digits." 
