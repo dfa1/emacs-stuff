2bfbaa3ad5bf7cb761c7c0d267f96f20fffe32dc
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-11 13:57:39 +0200

    ref: comment

--- 20-hg.el
+++ 20-hg.el
@@ -2,7 +2,7 @@
 ;; VC configuration as well as custom mercurial commands
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-09-11 13:41:47 dfa>
+;; Time-stamp: <2011-09-11 13:57:34 dfa>
 
 ;; automatically follow symlinks
 (setq vc-follow-symlinks t)
@@ -14,6 +14,6 @@
   (switch-to-buffer "*hg push*")
   (vc-do-command "*hg push*" `async' "hg" nil "push"))
 
-;; 
+;; use conf-mode for hg config files 
 (add-to-list 'auto-mode-alist '("hgrc" . conf-mode))
 (add-to-list 'auto-mode-alist '(".hgrc" . conf-mode))
