5212de4832f69cd0a171caf5927635f2355e9f55
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-11 13:43:03 +0200

    enh: use conf-mode for .hgrc/hgrc

--- 20-hg.el
+++ 20-hg.el
@@ -2,7 +2,7 @@
 ;; VC configuration as well as custom mercurial commands
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2010-12-29 16:57:18 dfa>
+;; Time-stamp: <2011-09-11 13:41:47 dfa>
 
 ;; automatically follow symlinks
 (setq vc-follow-symlinks t)
@@ -14,4 +14,6 @@
   (switch-to-buffer "*hg push*")
   (vc-do-command "*hg push*" `async' "hg" nil "push"))
 
-
+;; 
+(add-to-list 'auto-mode-alist '("hgrc" . conf-mode))
+(add-to-list 'auto-mode-alist '(".hgrc" . conf-mode))
