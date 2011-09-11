f155455402dc6b29acc2b8a91290cc5cdbf36674
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-11 13:53:41 +0200

    ref: removed graphviz-mode

--- 53-graphviz.el
+++
@@ -1,8 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; RPM spec mode
-;;
-;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-04-28 13:56:14 dangelocola>
-
-;; TODO: use autoload
-(load-library "graphviz-dot-mode")
