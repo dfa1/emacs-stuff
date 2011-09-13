944501713c72a37a3ee76e3b8d3b8ee18fc77b30
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-13 18:15:31 +0200

    ref: naming

--- 10-reindent.el
+++ 10-reindent.el
@@ -1,18 +1,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Re-indent whole buffer, mapped as in netbeans (alt-shift-f). 
+;; Indent buffer, mapped as in netbeans (alt-shift-f). 
 ;;
 ;; Author    : Davide Angelocola <davide.angelocola@gmail.com>
-;; Time-stamp: <2011-09-13 00:05:55 dfa>
+;; Time-stamp: <2011-09-13 18:13:28 dfa>
 ;;
 
-(defun reindent-whole-buffer ()
-  "Re-indent the whole buffer."
+(defun indent-buffer ()
+  "Indent the buffer according to the current major mode."
   (interactive)
   (save-excursion 
-    (with-temp-message "Indenting..."
+    (with-temp-message (format "indenting %s" (buffer-name))
       (indent-region (point-min) (point-max)))))
 
 ;; disable forward-word (I prefer C-<left>)
 (global-unset-key "\M-F")
 (global-unset-key "\M-f")
-(global-set-key "\M-F" 'reindent-whole-buffer)
+(global-set-key "\M-F" 'indent-buffer)
