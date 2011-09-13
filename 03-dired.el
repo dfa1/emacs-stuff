90bc943ee267d22ecfd8cd2e8ce05cfa45ae3855
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-13 20:15:49 +0200

    ref: removed useless dired configurations

--- 03-dired.el
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Fixing dired annoyances.
-
-(add-hook 'dired-mode-hook
-	  (lambda ()
-	    ;; re-using buffers 
-	    (put 'dired-find-alternate-file 'disabled nil)  
-	    ;; disabling opening of files/directories with mouse click
-	    (local-unset-key [mouse-2])))
-
