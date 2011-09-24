b39359b5c14b77652b37407b927367d0bec1f7f3
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-24 21:26:39 +0200

    enh: implemented switching between test/source using a custom layout src/foo.clj (ns foo) test/foo/test.clj (ns foo.test)

--- 42-clojure.el
+++ 42-clojure.el
@@ -25,7 +25,7 @@
   (interactive)
   (if (get-buffer "*slime-repl nil*") 
       (switch-to-buffer "*slime-repl nil*") 
-    (clojure-jack-in)))
+    (clojure-jack-i09n)))
 
 ;; redefine local key bindings (as in netbeans)
 (add-hook 'clojure-mode-hook
@@ -33,7 +33,7 @@
 	    (local-set-key (kbd "<C-f6>") 'clojure-test-run-tests)
 	    (local-unset-key "\C-T")
 	    (local-unset-key "\C-t")
-	    (local-set-key "\C-T" 'clojure-test-jump-to-implementation)))
+	    (local-set-key "\C-T" 'my-clojure-switch-implementation-test)))
 
 ;; skeletons.. OMG!
 (add-hook 'clojure-mode-hook
@@ -44,3 +44,30 @@
 	      "(is false))")
 	    (local-set-key "\C-cit" 'clojure-skeleton-deftest)
 	    (setq skeleton-end-hook nil)))
+
+;; my convention for clojure projects:
+;;   src/foo.clj
+;;   test/foo/test.clj
+
+(defun parent-directory-name (file-name)
+  (interactive)
+  (car (last (split-string (file-name-directory buffer-file-name) "/" t))))
+
+(defun my-clojure-jump-to-test (src-name)
+  (find-file 
+   (format 
+    "../test/%s/test.clj" 
+    (file-name-sans-extension (file-name-nondirectory src-name)))))
+
+(defun my-clojure-jump-to-implementation (test-name)
+  (interactive)
+  (find-file 
+   (format 
+    "../../src/%s.clj" 
+    (parent-directory-name (file-name-sans-extension test-name))))
+
+(defun my-clojure-switch-implementation-test ()
+  (interactive)
+  (if (string-match-p "test.clj" (file-name-nondirectory buffer-file-name))
+      (my-clojure-jump-to-implementation buffer-file-name)
+    (my-clojure-jump-to-test buffer-file-name)))
